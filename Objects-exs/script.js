cc17f35c467c92cd833d716e8429a7dd878a40f9
pollyblo <ugzcollet@gmail.com>
2023-01-28 19:34:31 +0100

    feat: addBook, work on deleteRow

--- Objects-exs/script.js
+++ Objects-exs/script.js
@@ -4,8 +4,13 @@ const nPages = document.getElementById('pages');
 const readCheck = document.getElementById('read-check');
 const saveButton = document.getElementById('save-btn');
 const tBody = document.getElementById('table');
+const tRows = document.getElementsByTagName('tr');
+const addBook = document.getElementById('add-book');
+const form = document.getElementById('form');
+const nError = document.getElementById('number-error');
+const deleteBtn = document.getElementsByClassName('delete-btn');
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, index) {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -14,46 +19,101 @@ function Book(title, author, pages, read) {
   } else {
     this.read = 'already read';
   }
+  this.index = index;
 }
 
 Book.prototype.info = function () {
   return `${this.title}, ${this.author}, ${this.pages}, ${this.read}.`;
 };
 
-const myLibrary = [
-  new Book('Promises', 'Boo Flee', '928', false),
-  new Book('Elze', 'Mektour Ahmed', '685', true),
-  new Book('Hall Pieces', 'Voisin Merche', '68', true),
-];
+let myLibrary = [];
 
-function addRow() {
+function deleteRow(index) {
+  const rowArray = Array.from(tRows).slice(1);
+  for (let i = 0; i <= rowArray.length - 1; i += 1) {
+    const idxRow = Number(rowArray[i].dataset.index);
+    console.log(i, rowArray[i].dataset.index);
+    if (idxRow === index) {
+      tBody.deleteRow(index);
+
+      console.log(rowArray);
+    }
+  }
+}
+
+function deleteBook() {
+  Array.from(deleteBtn).forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const idxRow = Number(e.target.dataset.index);
+      const newLib = myLibrary.reduce((arr, book) => {
+        if (book.index !== idxRow) {
+          arr.push({
+            ...book,
+            index: arr.length,
+          });
+        }
+        return arr;
+      }, []);
+      myLibrary = newLib;
+      deleteRow(idxRow);
+    });
+  });
+}
+
+function addRow(e) {
+  e.preventDefault();
+  tBody.innerHTML = '';
   myLibrary.forEach((book) => {
     const bookValues = Object.values(book);
     const newRow = tBody.insertRow(0);
+    const button = document.createElement('button');
     for (let i = 0; i < 4; i += 1) {
       const newCell = newRow.insertCell(i);
       const contentCell = document.createTextNode(bookValues[i]);
       newCell.appendChild(contentCell);
     }
+    button.setAttribute('class', 'delete-btn');
+    button.setAttribute('data-index', book.index);
+    button.textContent = 'Supprimer';
+    newRow.appendChild(button);
+    newRow.dataset.index = book.index;
   });
+  deleteBook();
 }
 
-function addBookToLibrary() {
-  let isInputFalse = false;
+function addBookToLibrary(e) {
+  e.preventDefault();
+  const isPageNumber = Number.isNaN(Number(nPages.value));
   const bookSaved = new Book(
     bookTitle.value,
     authorName.value,
     nPages.value,
-    readCheck.checked
+    readCheck.checked,
+    myLibrary.length
   );
   const arrValues = Object.values(bookSaved);
+  let isInputFalse = false;
   arrValues.forEach((val) => {
     if (val === '') {
       isInputFalse = true;
     }
   });
-  if (isInputFalse) return;
-  myLibrary.push(bookSaved);
+  if (isInputFalse || isPageNumber) return;
+  myLibrary.unshift(bookSaved);
+  addRow(e, myLibrary);
+  form.reset();
 }
 
-saveButton.addEventListener('click', addRow);
+saveButton.addEventListener('click', addBookToLibrary);
+
+addBook.addEventListener('click', () => {
+  form.style.display = 'flex';
+});
+
+nPages.addEventListener('input', (e) => {
+  if (Number.isNaN(Number(e.target.value))) {
+    nError.style.display = 'block';
+  } else {
+    nError.style.display = 'none';
+  }
+});
