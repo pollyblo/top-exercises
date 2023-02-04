be67558d815a11e2e6875ac51715b61a879f6758
pollyblo <ugzcollet@gmail.com>
2023-02-04 19:39:24 +0100

    feat: game system / restart btn

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -5,14 +5,24 @@ const gameBoard = (() => {
     }
     return board;
   };
+  const isFull = (board) => {
+    for (let i = 0; i < 9; i += 1) {
+      if (board[i] === '') {
+        return false;
+      }
+    }
+    return true;
+  };
   return {
     generateGameboard,
+    isFull,
   };
 })();
 
 const gameFlow = (() => {
   const turn = 0;
-  const actualBoard = gameBoard.generateGameboard();
+  const match = 0;
+  let actualBoard = gameBoard.generateGameboard();
   const isShotPossible = (n) => actualBoard[n] === '';
   const isWinning = (symbol) => {
     const combs = [
@@ -41,45 +51,74 @@ const gameFlow = (() => {
     }
     return counter === 3;
   };
+  const alterBoard = (n, player) => {
+    if (!isShotPossible(n)) {
+      return;
+    }
+    actualBoard[n] = player;
+  };
+  const resetGame = (isReset = false) => {
+    if (
+      gameBoard.isFull(actualBoard) ||
+      isWinning('X') ||
+      isWinning('O') ||
+      isReset
+    ) {
+      actualBoard = gameBoard.generateGameboard();
+    }
+  };
   return {
     actualBoard,
     turn,
+    match,
     isShotPossible,
     isWinning,
+    alterBoard,
+    resetGame,
   };
 })();
 
 const player = (whichPlayer) => {
   const board = gameFlow.actualBoard;
+  const win = 0;
   const getPlayer = () => whichPlayer;
-  const playerShot = (n) => {
-    if (!gameFlow.isShotPossible(n)) {
-      return;
-    }
-    board[n] = getPlayer();
-    gameFlow.actualBoard = board;
-    gameFlow.turn += 1;
-  };
   return {
     board,
+    win,
     getPlayer,
-    playerShot,
+    // playerShot,
   };
 };
 
-const rendering = (() => {
+const p1 = player('X');
+const p2 = player('O');
+
+const domRendering = (() => {
   const container = document.getElementById('container');
-  const modelBoard = gameFlow.actualBoard;
   const displayResult = () => {
-    if (gameFlow.isWinning('X')) {
-      console.log('Bravo X');
-    } else if (gameFlow.isWinning('O')) {
-      console.log('Bravo O');
+    const tieGame = document.getElementById('tie-game');
+    const oWin = document.getElementById('o-win');
+    const xWin = document.getElementById('x-win');
+    if (gameFlow.isWinning(p1.getPlayer())) {
+      xWin.style.display = 'block';
+      return true;
     }
+    if (gameFlow.isWinning(p2.getPlayer())) {
+      oWin.style.display = 'block';
+      return true;
+    }
+    if (gameBoard.isFull(gameFlow.actualBoard)) {
+      tieGame.style.display = 'block';
+      return true;
+    }
+    tieGame.style.display = 'none';
+    xWin.style.display = 'none';
+    oWin.style.display = 'none';
+    return false;
   };
   const displayBoard = () => {
     container.replaceChildren();
-    modelBoard.forEach((grid, i) => {
+    gameFlow.actualBoard.forEach((grid, i) => {
       const button = document.createElement('button');
       container.appendChild(button);
       button.classList.add('case');
@@ -87,9 +126,11 @@ const rendering = (() => {
       switch (grid) {
         case 'X':
           button.textContent = 'X';
+          button.style.color = '#1e549f';
           break;
         case 'O':
           button.textContent = 'O';
+          button.style.color = '#c13131';
           break;
         default:
           button.textContent = '';
@@ -97,26 +138,51 @@ const rendering = (() => {
     });
     listenerShot();
   };
+  const resetGame = (isReset) => {
+    if (displayResult() || isReset) {
+      gameFlow.resetGame(isReset);
+      gameFlow.actualBoard = gameBoard.generateGameboard();
+      gameFlow.turn = 0;
+    }
+  };
   const listenerShot = () => {
     const buttons = document.querySelectorAll('.case');
     buttons.forEach((btn) => {
       btn.addEventListener('click', (e) => {
-        if (gameFlow.turn % 2 === 0) {
-          p1.playerShot(e.target.value);
-        } else {
-          p2.playerShot(e.target.value);
+        if (!gameFlow.isShotPossible(e.target.value)) {
+          return;
         }
+        // eslint-disable-next-line no-unused-expressions
+        if (gameFlow.turn % 2 === 0) {
+          gameFlow.alterBoard(e.target.value, 'X');
+          gameFlow.actualBoard[e.target.value] = 'X';
+        } else {
+          gameFlow.alterBoard(e.target.value, 'O');
+          gameFlow.actualBoard[e.target.value] = 'O';
+        }
+        console.log('model board', gameFlow.actualBoard);
+        gameFlow.turn += 1;
+
         displayBoard();
-        displayResult();
+        resetGame();
       });
+    });
+  };
+  const restartOption = () => {
+    const button = document.createElement('button');
+    button.textContent = 'Restart';
+    button.setAttribute('id', 'restart-btn');
+    document.body.appendChild(button);
+    button.addEventListener('click', () => {
+      resetGame(true);
+      displayBoard();
     });
   };
   return {
     displayBoard,
-    modelBoard,
+    restartOption,
   };
 })();
 
-const p1 = player('X');
-const p2 = player('O');
-rendering.displayBoard();
+const newGame = domRendering.displayBoard();
+const rstButton = domRendering.restartOption();
